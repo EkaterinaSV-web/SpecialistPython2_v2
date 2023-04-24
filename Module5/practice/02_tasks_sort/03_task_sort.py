fa147af3a2f5c84f983f8a11b51c1bc53794bee4
EkaterinaSV-web <131470278+EkaterinaSV-web@users.noreply.github.com>
2023-04-24 19:16:01 +0300

    Update 03_task_sort.py

--- Module5/practice/02_tasks_sort/03_task_sort.py
+++ Module5/practice/02_tasks_sort/03_task_sort.py
@@ -3,4 +3,7 @@
 # Найти: сумму 5-ти самых больших элементов
 
 numbers = [-2.5, 13.6, -13, -22.4, -12.8, -6.7, 12.8, -21, 4, -22.1, 0]
+number_sorting = sorted(numbers, reverse=True)
+result = sum(number_sorting[:5])
 
+print(result)
